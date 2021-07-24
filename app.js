b080030c65f61a2c29d19dc9b8f5b1c1f98360f1
Mukhammadyusuf Abdurakhimov <Mukhammadyusuf_Abdurakhimov@epam.com>
2021-07-25 00:19:35 +0500

    Model setup + layout setup

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+if (process.env.NODE_ENV != 'production') {
+    require("dotenv").config();
+}
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
+
 
 //Routes
 const indexRouter = require('./routes/index')
@@ -15,6 +19,24 @@ app.use(express.static('public'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/', indexRouter)
+
+console.log(process.env.DATABASE_URL);
+
+//Model configs
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection
+db.on('error', (err) => {
+    console.error(err);
+})
+db.once('open', () => {
+    console.log("Databse connected");
+})
+
 
 
 app.listen(process.env.PORT || 3000, () => console.log('Server has been started'))
