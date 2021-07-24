b9bbe7c271ca75b558c6c8c802ec0725e0b828c0
Mukhammadyusuf Abdurakhimov <Mukhammadyusuf_Abdurakhimov@epam.com>
2021-07-24 23:50:57 +0500

    Routes are setted up

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
 
+//Routes
+const indexRouter = require('./routes/index')
+
 // Express configureations
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+
+app.use(expressLayouts)
 app.use(express.static('public'));
 app.use(express.json())
-app.use(express.urlencoded({ extended: ture }))
+app.use(express.urlencoded({ extended: false }))
+app.use('/', indexRouter)
 
 
-app.listen(3000, () => console.log('Server has been started'))
+app.listen(process.env.PORT || 3000, () => console.log('Server has been started'))
