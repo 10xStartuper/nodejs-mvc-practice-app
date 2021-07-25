94bc3879072693eaf73893ea6e7c01c5fc0f4ed7
Mukhammadyusuf Abdurakhimov <Mukhammadyusuf_Abdurakhimov@epam.com>
2021-07-25 18:21:40 +0500

    Some major updates

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 if (process.env.NODE_ENV != 'production') {
     require("dotenv").config();
 }
+
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
-
-
-//Routes
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
+
+
 
 // Express configureations
 app.set('view engine', 'ejs')
@@ -17,10 +18,12 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'));
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use('/', indexRouter)
+app.use(express.urlencoded({ limit: '10mb', extended: false }))
 
-console.log(process.env.DATABASE_URL);
+
+//Routes
+app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
 
 //Model configs
 const mongoose = require("mongoose");
@@ -34,7 +37,7 @@ db.on('error', (err) => {
     console.error(err);
 })
 db.once('open', () => {
-    console.log("Databse connected");
+    console.log("Databse open");
 })
 
 
